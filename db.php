faaa6c2231aed30de395310d6415f012f14bdeb7
Jie0109 <liyuguangjie@gmail.com>
2024-10-03 00:18:05 +0800

    change db name

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
     define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
-    define('DB_NAME', 'jobhunter');
+    define('DB_NAME', 'jobhunter');//change to your own database name
 
     /* Attempt to connect to MySQL database */
     $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
